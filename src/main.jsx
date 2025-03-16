bbd52a0f375bf41b88698090561c498fbc1a8f92
Harshal Vasudev Warke <156218037+Harshal6191@users.noreply.github.com>
2025-03-17 00:31:21 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   
